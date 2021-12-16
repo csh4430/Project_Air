5ade51aadd08172a6148c6aecf19e54f59c667ce
kjp29816265@gmail.com <kjp29816265@gmail.com>
2021-12-16 18:56:06 +0900

    에니메이션 바꾸는 코드 추가 아마도, 테스트를 해보지 않아서 모르겠네... 나중에 버그 나오면 그때 고침

--- Assets/02.Script/ShopManager.cs
+++ Assets/02.Script/ShopManager.cs
@@ -13,11 +13,11 @@ public class ShopManager : MonoBehaviour
         public Sprite skinImage;
         public bool hasSkin;
         public int cost;
-        //public AnimatorOverrideController skinAnimator_Blue;
-        //public AnimatorOverrideController skinAnimator_Green;
-        //public AnimatorOverrideController skinAnimator_Pink;
-        //public AnimatorOverrideController skinAnimator_Purple;
-        //public AnimatorOverrideController skinAnimator_Red;
+        public AnimatorOverrideController skinAnimator_Blue;
+        public AnimatorOverrideController skinAnimator_Green;
+        public AnimatorOverrideController skinAnimator_Pink;
+        public AnimatorOverrideController skinAnimator_Purple;
+        public AnimatorOverrideController skinAnimator_Red;
     }
 
     public List<ShopItems> shopItems = new List<ShopItems>();
@@ -25,6 +25,9 @@ public class ShopManager : MonoBehaviour
     private List<GameObject> shopItemGameObject = new List<GameObject>();
 
     public GameObject contentPosition = null;
+
+    [Tooltip("파랑, 초록, 핑크, 보라, 빨강 순으로 넣어야 함")]
+    public List<GameObject> units = new List<GameObject>();
 
     void Start()
     {
@@ -51,6 +54,19 @@ public class ShopManager : MonoBehaviour
         ListContentChange();
     }
 
+    void UnitAnimatorChange()
+    {
+        for (int i = 0; i < shopItemGameObject.Count; i++)
+        {
+            units[0].GetComponent<Animator>().runtimeAnimatorController = shopItems[i].skinAnimator_Blue.runtimeAnimatorController;
+            units[1].GetComponent<Animator>().runtimeAnimatorController = shopItems[i].skinAnimator_Green.runtimeAnimatorController;
+            units[2].GetComponent<Animator>().runtimeAnimatorController = shopItems[i].skinAnimator_Pink.runtimeAnimatorController;
+            units[3].GetComponent<Animator>().runtimeAnimatorController = shopItems[i].skinAnimator_Purple.runtimeAnimatorController;
+            units[4].GetComponent<Animator>().runtimeAnimatorController = shopItems[i].skinAnimator_Red.runtimeAnimatorController;
+        }
+    }
+
+
     void ListContentChange()
     {
         for(int i = 0; i < shopItemGameObject.Count; i++)
@@ -58,6 +74,7 @@ public class ShopManager : MonoBehaviour
             shopItemGameObject[i].transform.GetChild(0).GetComponent<Image>().sprite = shopItems[i].skinImage;
             shopItemGameObject[i].transform.GetChild(1).GetComponent<Text>().text = shopItems[i].skinName;
             shopItemGameObject[i].transform.GetChild(2).GetComponent<Text>().text = shopItems[i].cost.ToString();
+            shopItemGameObject[i].transform.GetChild(3).GetComponent<Button>().onClick.AddListener(() => UnitAnimatorChange());
         }
     }
 
